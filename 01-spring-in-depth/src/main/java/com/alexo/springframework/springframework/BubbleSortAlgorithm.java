25b68654629fe2b0f1ee41b79cae69ac54802c1c
Alexo Ballester <alexander.ballester@orbium.com>
2018-07-13 17:20:07 +0800

    Dynamic autowiring and troubleshooting -@primary

--- 01-spring-in-depth/src/main/java/com/alexo/springframework/springframework/BubbleSortAlgorithm.java
+++ 01-spring-in-depth/src/main/java/com/alexo/springframework/springframework/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.alexo.springframework.springframework;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm{
 
     public int[] sort(int[] numbers) {
--- 01-spring-in-depth/src/main/java/com/alexo/springframework/springframework/QuickSortAlgorithm.java
+++ 01-spring-in-depth/src/main/java/com/alexo/springframework/springframework/QuickSortAlgorithm.java
@@ -1,5 +1,9 @@
 package com.alexo.springframework.springframework;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int[] numbers) {
